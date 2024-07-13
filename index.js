8cef015f8b743c1013b8ca9940095196fa2e5a3d
Amar Nayak <93204669+AMARNAYAK7262@users.noreply.github.com>
2024-07-13 16:56:41 +0530

    Adding all the final files
    The project is Completed

---
+++ index.js
@@ -0,0 +1,23 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_hpla15j";
+  const templateID = "template_i3jmexm";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
+
